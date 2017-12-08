bca443db964d426ad0e49e64ee61a4a138dc4420
shashaankunt <32075174+shashaankunt@users.noreply.github.com>
2017-12-08 13:21:00 -0600

    Updating IntList.cpp and added IntList.h

--- Intlist.cpp
+++ Intlist.cpp
@@ -1,19 +1,7 @@
 
-#include <bits/stdc++.h>
-using namespace std;
-class IntList{
-private:
-   int head=0;
-   int tail=0;
-   int abc=0;
-  IntList(int head) ;
-  IntList( int X,  int Xs) ;
-  static IntList cons(int X,int Xs);
-  static IntList app(int y ,int Ys);
-  static int toInts(int Xs);
-  static int len(int Xs);
-};
-int main()
-{
-   return 0;
-}
+#include "IntList.h"
+
+
+
+IntList* IntList::empty = nullptr;
+
