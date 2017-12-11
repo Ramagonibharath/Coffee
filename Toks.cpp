a64d9ecd065d87f7fb2d99909c3ce66e809cead5
anuragbandla <32075622+anuragbandla@users.noreply.github.com>
2017-12-10 23:31:41 -0600

    Create Toks.cpp

---
+++ Toks.cpp
@@ -0,0 +1,8 @@
+#include "Toks.h"
+
+std::string Toks::IF = "if";
+std::string Toks::AND = "and";
+std::string Toks::DOT = ".";
+std::string Toks::HOLDS = "holds";
+std::string Toks::LISTS = "lists"; // todo
+std::string Toks::IS = "is"; // todo
