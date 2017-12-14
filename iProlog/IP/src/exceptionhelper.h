9adabc2496b1329244647306c31cc301e28e81ee
sai ganesh <SAIGANESH.DHATRIKA@GMAIL.COM>
2017-12-13 21:54:17 -0600

    Create exceptionhelper.h

---
+++ iProlog/IP/src/exceptionhelper.h
@@ -0,0 +1,55 @@
+#ifndef EXCEPTIONHELPER
+#define EXCEPTIONHELPER
+
+#include <stdexcept>
+
+class NullPointerException : public std::exception
+{
+private:
+    std::string msg;
+
+public:
+    NullPointerException(const std::string& message = "") : msg(message)
+    {
+    }
+
+    const char * what() const throw()
+    {
+        return msg.c_str();
+    }
+};
+
+class IllegalStateException : public std::exception
+{
+private:
+    std::string msg;
+
+public:
+    IllegalStateException(const std::string& message = "") : msg(message)
+    {
+    }
+
+    const char * what() const throw()
+    {
+        return msg.c_str();
+    }
+};
+
+class IOException : public std::exception
+{
+private:
+    std::string msg;
+
+public:
+    IOException(const std::string& message = "") : msg(message)
+    {
+    }
+
+    const char * what() const throw()
+    {
+        return msg.c_str();
+    }
+};
+
+
+#endif	//#ifndef EXCEPTIONHELPER
